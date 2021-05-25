dbaaa4dff69dbd44ea425ba19fb8eb507e8605c2
Christian Micklisch <christian.micklisch@icloud.com>
2021-05-24 23:24:09 -0400

    Updated jupyter remote script to handle docker container

--- jupyter-remote.sh
+++ jupyter-remote.sh
@@ -16,6 +16,15 @@ cleanup() {
     exit $rv
 }
 
+cleanupDocker() {
+    printf "${RED}\n\nCLEANING UP DOCKER JUPYTER REMOTELY for ${1}@${2} docker ${3}\n\n${NC}"
+    rv=$?
+    ssh $1@$2 "docker exec ${3} python3 -m notebook stop 8889"
+    ssh $1@$2 "docker container stop ${3}"
+    printf "\n\n${GREEN}JUPYTER EXITED!${NC}\n\n"
+    exit $rv
+}
+
 echo -n "USERNAME [root]: "
 
 read USERNAME
@@ -24,10 +33,32 @@ echo -n "HOST [192.39.192.2]: "
 
 read HOST
 
-trap "cleanup $USERNAME $HOST" EXIT
+echo -n "Use a docker container? [y/N]: "
 
-OUT=$(ssh $USERNAME@$HOST 'nohup python3 -m notebook --no-browser --port=8889 --ip=0.0.0.0 --allow-root > jupyter.log & echo $!> pid.txt')
-JUPYTER_OUTPUT=$(ssh $USERNAME@$HOST 'python3 -m notebook list')
+read USE_DOCKER_CONTAINER
+
+if [ "$USE_DOCKER_CONTAINER" == "y" ]; then
+    
+    echo -n "DOCKER NAME [brats]: "
+
+    read DOCKER_NAME
+
+    echo -n "DOCKER IMAGE [nvidia/cuda]: "
+
+    read DOCKER_IMAGE
+
+    trap "cleanupDocker $USERNAME $HOST $DOCKER_NAME" EXIT
+
+    ssh $USERNAME@$HOST "docker run -d -it --rm -p 8889:8889 --name ${DOCKER_NAME} ${DOCKER_IMAGE}"
+    OUT=$(ssh $USERNAME@$HOST "docker exec ${DOCKER_NAME} nohup python3 -m notebook --no-browser --port=8889 --ip=0.0.0.0 --allow-root > jupyter.log & echo $!> pid.txt")
+    JUPYTER_OUTPUT=$(ssh $USERNAME@$HOST "docker exec ${DOCKER_NAME} python3 -m notebook list")
+else
+    trap "cleanup $USERNAME $HOST" EXIT
+
+    OUT=$(ssh $USERNAME@$HOST 'nohup python3 -m notebook --no-browser --port=8889 --ip=0.0.0.0 --allow-root > jupyter.log & echo $!> pid.txt')
+    JUPYTER_OUTPUT=$(ssh $USERNAME@$HOST 'python3 -m notebook list')
+fi
+
 URL=$(echo $JUPYTER_OUTPUT | sed 's/Currently running servers: //g' | sed 's/ ::.*//g')
 ssh -fNT -L 8889:localhost:8889 $USERNAME@$HOST
 
